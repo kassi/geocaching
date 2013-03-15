34b1ec064e13d2ad283cb9d531f03e59e382e9fe
Karsten Silkenbäumer <ksi@kluks.de>
2013-03-15 15:16:12 +0100

    Add Integer.crossfoot method

--- lib/core_ext/Integer.rb
+++ lib/core_ext/Integer.rb
@@ -1,5 +1,17 @@
 class Integer
+  class NegativeIntegerError < StandardError; end
+
   def digits
     self.to_s.length
+  end
+
+  def crossfoot
+    raise NegativeIntegerError if self < 0
+
+    result = 0
+    self.to_s.chars.each do |char|
+      result += char.to_i
+    end
+    result
   end
 end
--- test/core_ext/Integer_test.rb
+++ test/core_ext/Integer_test.rb
@@ -2,15 +2,40 @@ require 'test_helper'
 
 module CoreExt
   module Integer
+
     class DigitsTest < Test::Unit::TestCase
+
       test 'should calculate length of 1 digit number' do
         assert_equal 1, 1.digits
       end
-      test 'should calculate length of 2 number' do
+
+      test 'should calculate length of 2 digit number' do
         assert_equal 2, 10.digits
       end
+
       test 'should calculate length of negative number' do
         assert_equal 3, -10.digits
+      end
+    end
+
+    class CrossfootTest < Test::Unit::TestCase
+
+      test 'should calculate crossfoot of 1 digit number' do
+        assert_equal 5, 5.crossfoot
+      end
+
+      test 'should calculate crossfoot of 2 digit number' do
+        assert_equal 6, 15.crossfoot
+      end
+
+      test 'should calculate 2 digit crossfoot' do
+        assert_equal 13, 94.crossfoot
+      end
+
+      test 'should raise Exception if number is negative' do
+        assert_raise ::Integer::NegativeIntegerError do
+          -1.crossfoot
+        end
       end
     end
   end
