d399b52e1532c87e79c761b8a486419a7dda2058
Karsten Silkenbäumer <ksi@kluks.de>
2013-03-16 00:36:59 +0100

    Add String.caesar method

--- lib/core_ext/string.rb
+++ lib/core_ext/string.rb
@@ -12,4 +12,19 @@ class String
     end
     result
   end
+
+  def caesar(rot=3)
+    result = ""
+    self.chars.each do |char|
+      char.upcase!
+      if char.between? "A", "Z"
+        code0 = char.ord-65
+        code0 = (code0 + rot) % 26
+        result += (code0+65).chr
+      else
+        result += char
+      end
+    end
+    result
+  end
 end
--- test/core_ext/string_test.rb
+++ test/core_ext/string_test.rb
@@ -17,6 +17,33 @@ module CoreExt
         assert_equal [20,5,19,20," ",9,20], "TEST IT".to_a1b2
       end
     end
+
+    class CaesarTest < Test::Unit::TestCase
+
+      test 'should rotate all characters about 3' do
+        assert_equal "DEFGH", "ABCDE".caesar
+      end
+
+      test 'should rotate around the border' do
+        assert_equal "YZABC", "VWXYZ".caesar
+      end
+
+      test 'should not change non characters' do
+        assert_equal "DEF 1 GH", "ABC 1 DE".caesar
+      end
+
+      test 'should treat lower case letters like upper case letters' do
+        assert_equal "DEF 1 GH", "Abc 1 De".caesar
+      end
+
+      test 'should get step size as optional parameter' do
+        assert_equal "EFGHI", "ABCDE".caesar(4)
+      end
+
+      test 'should allow negative step size' do
+        assert_equal "YZABC", "ABCDE".caesar(-2)
+      end
+    end
   end
 end
 
